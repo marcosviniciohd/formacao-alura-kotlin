8658ea6f63857afd69bb5552b97a5f65cafb263f
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-05-18 08:36:31 -0300

    feature: endpoint buscaPorId

--- forum/forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ forum/forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -1,10 +1,9 @@
 package br.com.alura.forum.controller
 
-import br.com.alura.forum.model.Curso
 import br.com.alura.forum.model.Topico
-import br.com.alura.forum.model.Usuario
 import br.com.alura.forum.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,8 +11,15 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/topicos")
 class TopicoController(private val service: TopicoService) {
 
-        @GetMapping
-        fun listar(): List<Topico> {
-            return service.listar()
-        }
+    @GetMapping
+    fun listar(): List<Topico> {
+        return service.listar()
+    }
+
+    @GetMapping("/{id}")
+    fun buscarPorId(@PathVariable id: Long): Topico {
+        return service.buscarPorId(id)
+    }
+
+
 }
--- forum/forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ forum/forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -4,10 +4,12 @@ import br.com.alura.forum.model.Curso
 import br.com.alura.forum.model.Topico
 import br.com.alura.forum.model.Usuario
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
-class TopicoService {
-    fun listar(): List<Topico> {
+class TopicoService(private var topicos: List<Topico>) {
+
+    init {
         val topico = Topico(
             id = 1,
             titulo = "Duvida",
@@ -24,7 +26,47 @@ class TopicoService {
             )
         )
 
-        return listOf(topico, topico, topico)
+        val topico2 = Topico(
+            id = 2,
+            titulo = "Duvida2",
+            mensagem = "Duvida com Spring Data JPA",
+            curso = Curso(
+                id = 2,
+                nome = "Java",
+                categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Maria das Dores",
+                email = "maria.dores@gmail.com"
+            )
+        )
+
+        val topico3 = Topico(
+            id = 3,
+            titulo = "Duvida3",
+            mensagem = "Duvida com Spring Boot",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Antonio Silva",
+                email = "antonio.silva@gmail.com"
+            )
+        )
+
+       topicos = Arrays.asList(topico, topico2, topico3)
+    }
+
+    fun listar(): List<Topico> {
+        return topicos
+    }
+
+    fun buscarPorId(id: Long): Topico {
+        return topicos.stream().filter { t -> t.id == id }.findFirst().get()
     }
 
 }
