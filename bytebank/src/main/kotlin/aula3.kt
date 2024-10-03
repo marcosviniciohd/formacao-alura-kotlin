821c10b58e45a2b4f2484d0e19c1b34e200d2fa2
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-10-03 14:10:22 -0300

    feat: aula 3, introducao a metodos.

---
+++ bytebank/src/main/kotlin/aula3.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println("Bem Vindo ao Bytebank")
+    val conta1 = Conta()
+    conta1.titular = "Marcos Vinício"
+    conta1.numero = 1006
+    conta1.saldo = 00.0
+
+    val conta2 = Conta()
+    conta2.titular = "Daniela Oliveira"
+    conta2.numero = 1007
+    conta2.saldo = 00.0
+
+    deposita(conta1, 100.0)
+    deposita(conta2, 200.0)
+    println("Saldo da conta ${conta1.numero}, é de: ${conta1.saldo} reais")
+    println("Saldo da conta ${conta2.numero}, é de: ${conta2.saldo} reais")
+
+}
