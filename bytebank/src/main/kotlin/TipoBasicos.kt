d7b45da5efcc036fcc7c2be38123139c8f96ed76
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-10-07 10:24:37 -0300

    feat: documentacao tipos basicos

---
+++ bytebank/src/main/kotlin/TipoBasicos.kt
@@ -0,0 +1,27 @@
+fun main() {
+//   val d: Int
+//    10.also { d = it }
+//    println(d)
+//
+//    val e: String = "Hello World"
+//    println(e)
+
+//    val f: Int
+//    println(f) // Error: Variable 'f' must be initialized
+
+    val a: Int = 1000
+    val b: String = "log message"
+    val c: Double = 100.00
+    val d: Long = 100_000_000_000L
+    val e: Boolean = false
+    val f: Char = '\n'
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+
+
+}
