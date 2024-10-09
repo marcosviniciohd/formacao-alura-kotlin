3a5dcee5c795dc543ba3335179ef38b21f9aafad
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-10-09 10:54:40 -0300

    feat: documentacao estrutura de repeticao while e do while

---
+++ bytebank/src/main/kotlin/While.kt
@@ -0,0 +1,18 @@
+fun comerUmBolo() = println("Comer um bolo")
+fun assarUmBolo() = println("Assar um bolo")
+
+fun main() {
+    var bolosComidos = 0
+    var bolosAssados = 0
+
+    while (bolosComidos < 5) {
+        comerUmBolo()
+        bolosComidos ++
+
+    }
+
+    do {
+        assarUmBolo()
+        bolosAssados++
+    } while (bolosAssados < bolosComidos)
+}
