4f7fdbb2e6819bf3b8002dee95088f51beb8079a
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-10-11 18:39:31 -0300

    feat: documentacao range com if

---
+++ bytebank/src/main/kotlin/IfRange.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val x = 2
+    if (x in 1..5) {
+        println("$x está no range entre 1 e 5")
+    }
+
+    if (x !in 6..10) {
+        println("$x não está no range entre 6 e 10")
+    }
+}
