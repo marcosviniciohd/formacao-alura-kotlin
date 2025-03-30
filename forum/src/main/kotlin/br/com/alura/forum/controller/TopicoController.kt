893f038f346ad25cc1d4e7ebacb0b282de4bb1b8
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2025-03-30 14:17:08 -0300

    feat: add controller topico

---
+++ forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -0,0 +1,34 @@
+package br.com.alura.forum.controller
+
+import br.com.alura.forum.model.Curso
+import br.com.alura.forum.model.Topico
+import br.com.alura.forum.model.Usuario
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/topicos")
+class TopicoController {
+
+    @GetMapping
+    fun listar(): List<Topico> {
+        val topico = Topico(
+            id = 1,
+            titulo = "Dúvida Kotlin",
+            mensagem = "Variáveis no Kotlin",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Marcos Vinício",
+                email = "marcosviniciohd@gmail.com"
+            ),
+
+        )
+        return listOf(topico, topico, topico)
+    }
+}
